a14cda360623bdab2a629802138b1e1902b47a61
siva9192 <barinepallisiva@gmail.com>
2021-04-14 18:44:33 +0530

    sigle page application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home',  component: HomeComponent },
+  { path: 'menu',  component: MenuComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact',component: ContactComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/menu', pathMatch: 'full' },
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
+  { path: '', redirectTo: '/contact', pathMatch: 'full' }
 
+
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+
+}
